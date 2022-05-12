d7bf0e910f61f7593ab89e5d5467f464b2a5002b
Vikt0r <vbachmanov@mail.ru>
2022-05-12 20:26:22 +0300

    Add getAllContacts() helper function

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
 
+const https = require('https');
+const fs = require('fs');
+
 @Injectable()
 export class ContactService {
   create(createContactDto: CreateContactDto) {
@@ -17,7 +20,10 @@ export class ContactService {
   // }
   findOne(query: any) {
     console.log('Handle request');
-    return `This action returns the contact and ${query.add}`;
+
+    getAllContacts();
+
+    //return `This action returns the contact and ${query.add}`;
   }
 
   update(id: number, updateContactDto: UpdateContactDto) {
@@ -27,4 +33,56 @@ export class ContactService {
   remove(id: number) {
     return `This action removes a #${id} contact`;
   }
+}
+
+// helper functions
+
+function getAllContacts() {
+  console.log('\ngetAllContacts: \n');
+
+  const data = fs.readFileSync('private/file.txt', 'utf8');
+  const accessToken = JSON.parse(data).access_token;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  const callback = function (response) {
+    console.log('Headers: ', response.headers);
+    var str = '';
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    response.on('end', function () {
+      console.log('response end: ');
+      //console.log(str);
+
+      const resBody = JSON.parse(str);
+      const contacts = resBody._embedded.contacts;
+      console.log('Contacts: ', contacts);
+      contacts.forEach((contact) => {
+        const customFields = contact.custom_fields_values;
+        console.log('\ncustom_fields_values: ', customFields);
+        if (customFields) {
+          customFields.forEach((field) => {
+            console.log(field.values);
+          });
+        }
+      });
+    });
+  };
+
+  const url = 'https://vbachmanovmailru.amocrm.ru/api/v4/contacts';
+  const req = https.request(url, options, callback);
+
+  // req.write(
+  //   JSON.stringify({
+  //     with: 'leads',
+  //   }),
+  // );
+  req.end();
 }
