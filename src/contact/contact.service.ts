25f4ff6ef6115450ed2a4d16b8f8cddf9f0a4dc7
Vikt0r <vbachmanov@mail.ru>
2022-05-13 11:31:12 +0300

    Add findContactByQueryParam() function

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -21,33 +21,36 @@ export class ContactService {
   async findOne(query: any) {
     console.log('Handle request');
 
-    const allContacts: Array<any> = await new Promise((resolve, reject) => {
-      getAllContacts(resolve, reject);
-    });
+    // const allContacts: Array<any> = await new Promise((resolve, reject) => {
+    //   getAllContacts(resolve, reject);
+    // });
 
-    console.log('All contacts: ', allContacts);
-    allContacts.forEach((contact) => {
-      const customFields = contact.custom_fields_values;
-      console.log('\ncustom_fields_values: ', customFields);
+    // console.log('All contacts: ', allContacts);
+    // allContacts.forEach((contact) => {
+    //   const customFields = contact.custom_fields_values;
+    //   console.log('\ncustom_fields_values: ', customFields);
 
-      if (!customFields) {
-        return;
-      }
+    //   if (!customFields) {
+    //     return;
+    //   }
 
-      customFields.forEach((field) => {
-        console.log(field.values);
-      });
+    //   customFields.forEach((field) => {
+    //     console.log(field.values);
+    //   });
 
-      const compareResult = customFields.some((field) => {
-        console.log('field_code: ', field.field_code);
-        return field.values.some((valueObj) => {
-          console.log('field_value: ', valueObj.value);
-          console.log('query_value: ', query[field.field_code]);
-          return valueObj.value === query[field.field_code];
-        });
-      });
-      console.log('compareResult: ', compareResult);
-    });
+    //   const compareResult = customFields.some((field) => {
+    //     console.log('field_code: ', field.field_code);
+    //     return field.values.some((valueObj) => {
+    //       console.log('field_value: ', valueObj.value);
+    //       console.log('query_value: ', query[field.field_code]);
+    //       return valueObj.value === query[field.field_code];
+    //     });
+    //   });
+    //   console.log('compareResult: ', compareResult);
+    // });
+
+    let paramName = 'PHONE';
+    findContactByQueryParam(paramName, query[paramName]);
 
     //return `This action returns the contact and ${query.add}`;
   }
@@ -89,16 +92,7 @@ async function getAllContacts(resolve, reject) {
 
       const resBody = JSON.parse(str);
       const contacts = resBody._embedded.contacts;
-      // console.log('Contacts: ', contacts);
-      // contacts.forEach((contact) => {
-      //   const customFields = contact.custom_fields_values;
-      //   console.log('\ncustom_fields_values: ', customFields);
-      //   if (customFields) {
-      //     customFields.forEach((field) => {
-      //       console.log(field.values);
-      //     });
-      //   }
-      // });
+
       resolve(contacts);
     });
   };
@@ -106,10 +100,45 @@ async function getAllContacts(resolve, reject) {
   const url = 'https://vbachmanovmailru.amocrm.ru/api/v4/contacts';
   const req = https.request(url, options, callback);
 
-  // req.write(
-  //   JSON.stringify({
-  //     with: 'leads',
-  //   }),
-  // );
   req.end();
+}
+
+function findContactByQueryParam(paramName: string, paramValue: string) {
+  console.log('\nfindContactByQueryParam: \n');
+
+  return new Promise((resolve, reject) => {
+    const data = fs.readFileSync('private/file.txt', 'utf8');
+    const accessToken = JSON.parse(data).access_token;
+
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+
+    const callback = function (response) {
+      var str = '';
+      response.on('data', function (chunk) {
+        str += chunk;
+      });
+
+      response.on('end', function () {
+        console.log('Response end: ');
+        console.log('Status code: ', response.statusCode);
+        console.log('Headers: ', response.headers);
+        console.log(str);
+
+        // const resBody = JSON.parse(str);
+        // const contacts = resBody._embedded.contacts;
+
+        resolve(str);
+      });
+    };
+
+    const url = `https://vbachmanovmailru.amocrm.ru/api/v4/contacts?query=${paramValue}`;
+    const req = https.request(url, options, callback);
+
+    req.end();
+  });
 }
